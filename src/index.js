6a6d810f433da56dfe90390d7f13e7e01373340d
Mircea <frandesmyrce@gmail.com>
2018-11-14 21:55:08 +0200

    last update

---
+++ src/index.js
@@ -0,0 +1,28 @@
+function showHomePage() {
+    document.getElementById("skills-page").style.display = "none";
+    document.getElementById("languages-page").style.display = "none";
+    document.getElementById("home-page").style.display = "block";
+    console.info("Click on home");
+  }
+  
+  function showSkillsPage() {
+    document.getElementById("home-page").style.display = "none";
+    document.getElementById("languages-page").style.display = "none";
+    document.getElementById("skills-page").style.display = "block";
+    console.info("Click on skils");
+  }
+  
+  function showLanguagesPage() {
+    document.getElementById("home-page").style.display = "none";
+    document.getElementById("skills-page").style.display = "none";
+    document.getElementById("languages-page").style.display = "block";
+    console.info("Click on languages");
+  }
+  
+  document.getElementById("home-menu").onclick = showHomePage;
+  document.getElementById("skills-menu").onclick = showSkillsPage;
+  document.getElementById("languages-menu").onclick = showLanguagesPage;
+  
+  
+  
+  
