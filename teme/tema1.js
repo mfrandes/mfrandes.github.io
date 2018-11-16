390efcb48272da3ab06ef2c2469beb8a717225c8
Mircea <frandesmyrce@gmail.com>
2018-11-17 01:36:12 +0200

    m

--- teme/tema1.js
+++ teme/tema1.js
@@ -27,31 +27,32 @@
 
 // counter(7, 10);
 
-function counter(a, b) {
+function learningToAdd(a, b) {
     var sum = a + b;
+    
+    console.info ("Learning to add " + a + ` with ` + b );
 
     for (var i = 0; i < sum; i++) {
         var stringOfOne = ''; //se reseteaza la fiecare iteratie
-        if (b - i <= 0) {
-            console.log(`Jorge has ${sum} apples + Ethan has ${0} apples = now Jorge has all ${sum} apples, `);
-        }
+        
         if (b - i > 0) {
-            console.log(`Jorge has ${a + i} apples + Ethan has ${b - i} apples = ${sum} apples, if we take from Ethan one apple and we give it to Jorge, than...`); //aici am folosit 'template strings'
+            console.log(`Jorge has ${a + i} apples + Ethan has ${b - i} apples = in total they have ${sum} apples, if we take from Ethan one apple and we give it to Jorge, than...`); //aici am folosit 'template strings'
 
             for (var j = 0; j < b - i; j++) {
                 stringOfOne += ' + 1 apple'
             }
             console.log(`${(a + i) + " apples"}${ "+ 1 apple"} = ${a +i + 1} apples, `);
-           
+            
 			//in caz ca vrem sa evitam ultima consola, pt ca e
 			//acelasi lucru cu cea de mai sus, atunci:
 			//if (b - i >= 2) {
 			//  console.log(`${a + i}${stringOfOne} = ${sum}`);
             //}
-         
         
         }
+    
     }
+console.log(`Jorge has ${sum} apples + Ethan has ${0} apples = now Jorge has all ${sum} apples, `);    
 }
 
-counter(7, 10);
+learningToAdd(6, 4);
