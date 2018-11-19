7cf81deb708ed8baae2c427cd9ac4df79c997577
Mircea <frandesmyrce@gmail.com>
2018-11-19 19:08:54 +0200

    add show/hide function

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
+function hide(id) {
+  document.getElementById(id).style.display = "none";
+}
+function show(id) {
+  document.getElementById(id).style.display = "block";
+}
+
 function showHomePage() {
-    document.getElementById("skills-page").style.display = "none";
-    document.getElementById("languages-page").style.display = "none";
-    document.getElementById("home-page").style.display = "block";
-    console.info("Click on home");
-  }
-  
-  function showSkillsPage() {
-    document.getElementById("home-page").style.display = "none";
-    document.getElementById("languages-page").style.display = "none";
-    document.getElementById("skills-page").style.display = "block";
-    console.info("Click on skils");
-  }
-  
-  function showLanguagesPage() {
-    document.getElementById("home-page").style.display = "none";
-    document.getElementById("skills-page").style.display = "none";
-    document.getElementById("languages-page").style.display = "block";
-    console.info("Click on languages");
-  }
-  
-  document.getElementById("home-menu").onclick = showHomePage;
-  document.getElementById("skills-menu").onclick = showSkillsPage;
-  document.getElementById("languages-menu").onclick = showLanguagesPage;
-  
-  
-  
-  
+  hide("skills-page");
+  hide("languages-page");
+  show("home-page");
+  console.info("Click on home");
+}
+
+function showSkillsPage() {
+  hide("home-page");
+  hide("languages-page");
+  show("skills-page");
+  console.info("Click on skils");
+}
+
+function showLanguagesPage() {
+  hide("home-page");
+  hide("skills-page");
+  show("languages-page");
+  console.info("Click on languages");
+}
+
+document.getElementById("home-menu").onclick = showHomePage;
+document.getElementById("skills-menu").onclick = showSkillsPage;
+document.getElementById("languages-menu").onclick = showLanguagesPage;
+
+
+
