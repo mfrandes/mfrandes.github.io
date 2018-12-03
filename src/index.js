67980f14d6a5110a3598a1507f7f38110755f290
Mircea <frandesmyrce@gmail.com>
2018-12-03 21:08:37 +0200

    use jquery

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
-function $(id) {
-  return document.getElementById(id);
-}
-function hide(id) {
-  $(id).style.display = "none";
-}
-function show(id) {
-  $(id).style.display = "block";
-}
-
-function hideElement(el) {
-  el.style.display = 'none';
-}
-
-function hideAllPages() {
-  var pages = document.querySelectorAll(".page-block");
-  pages.forEach(hideElement);
-}
-
 function initMenu() {
   var links = document.querySelectorAll("#top-menu-bar a");
   for (var i = 0; i < links.length; i++) {
     links[i].onclick = function () {
-      hideAllPages();
+      $('.page-block').hide();
       var page = this.getAttribute('data-page');
-      show(page + "-page");
+      $('#' + page + "-page").fadeIn();
     };
   }
 }
@@ -53,5 +34,5 @@ function initSkillsPage() {
 }
 
 initMenu();
-show("skills-page");
+$('#skills-page').fadeIn();
 initSkillsPage();
